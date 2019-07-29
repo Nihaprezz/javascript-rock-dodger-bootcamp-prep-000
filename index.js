6ac99c1813212d83122e1b590c29f72e83f25a3c
Sergio Perez <sergio13prez@gmail.com>
2019-07-29 00:38:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -127,6 +127,7 @@ function moveDodger(e) {
    if(e.which === LEFT_ARROW){
      moveDodgerLeft();
      e.stopPropagation();
+     e.preventDefault();
    } else if (e.which === RIGHT_ARROW){
      moveDodgerRight();
    }
