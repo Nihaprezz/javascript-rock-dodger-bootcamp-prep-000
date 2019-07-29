eee97faf4b0cb1d2e61532ab8409d7ebd24e1e10
Sergio Perez <sergio13prez@gmail.com>
2019-07-29 00:47:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -114,8 +114,8 @@ function createRock(x) {
 function endGame() {
   clearIntervel(gameInterval);
   
-  for(var i = 0; i < rocks.length; i++){
-    rocks.remove();
+  for(var i = 0; i < ROCKS.length; i++){
+    ROCKS.remove();
   }
   
 }
